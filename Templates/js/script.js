cde9b5b934dbc30709148bb09de2225a2f427d20
default-007 <brianokola@gmail.com>
2023-07-13 20:28:34 +0300

    Project setup

---
+++ Templates/js/script.js
@@ -0,0 +1,28 @@
+//  $(document).ready(function () {
+//     $("#profile").click(function () {
+//         $("#profile").hide();
+//         $(".prof").show();
+//     });
+//     $(".prof").click(function () {
+//         $(".prof").hide();
+//         $("#profile").show();
+//     });
+
+//     $("#leave").click(function () {
+//         $("#leave").hide();
+//         $(".lv").show();
+//     });
+//     $(".lv").click(function () {
+//         $(".lv").hide();
+//         $("#leave").show();
+//     });
+
+//     $("#official").click(function () {
+//         $("#official").hide();
+//         $(".finish").show();
+//     });
+//     $(".finish").click(function () {
+//         $(".finish").hide();
+//         $("#official").show();
+//     });
+// });
